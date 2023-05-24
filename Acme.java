f88247b9736d0ee53dfa986c8083e1f5c01ab1f9
CHENTHILSP <132836556+CHENTHILSP@users.noreply.github.com>
2023-05-24 22:00:39 +0530

    Add files via upload

---
+++ Acme.java
@@ -0,0 +1,31 @@
+package Week3.day3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Acme {
+
+	public static void main(String[] args) throws InterruptedException {
+		EdgeDriver driver = new EdgeDriver();
+		driver.get("https://acme-test.uipath.com/login");
+		driver.manage().window().maximize();
+		WebElement email = driver.findElement(By.id("email"));
+		email.sendKeys("kumar.testleaf@gmail.com");
+		WebElement password = driver.findElement(By.name("password"));
+		password.sendKeys("leaf@12");
+		WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+		loginButton.click();
+		String welcomepagetitle = driver.getTitle();
+		System.out.println("title : " + welcomepagetitle);
+		WebElement logout = driver.findElement(By.xpath("//*[@id=\"bs-example-navbar-collapse-1\"]/ul/li[3]/a"));
+		logout.click();
+		Thread.sleep(2000);
+		driver.close();
+		
+		
+
+	}
+}
+
+
